65274e990cc07875b7f8640df3ea7780c84b0d65
Dhruba-jyoti-dutta <sddhruba.857@gmail.com>
2022-12-29 23:46:10 +0530

    first commit

---
+++ core/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+import string
+
+# Create your views here.
+
+
+def get_html_content(request):
+    import requests
+    city = request.GET.get('city')
+    name1=string.capwords(city)
+    lst_name=name1.split()
+    fnl_name='_'.join(lst_name)
+    url="https://en.wikipedia.org/wiki/"+fnl_name
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    html_content = session.get(url).text
+    return html_content
+
+
+def home(request):
+    result = None
+    if 'city' in request.GET:
+        # fetch the weather from Google.
+        html_content = get_html_content(request)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+        result = dict()
+        result['city'] = request.GET.get('city')
+        result['short_description']=soup.find('div',class_='shortdescription nomobile noexcerpt noprint searchaux').text
+        details=soup.find_all('table',class_='infobox')
+        list1=[]
+        list2=[]
+        for detail in details:
+            h=detail.find_all('tr')
+            for j in h:
+                heading=j.find_all('th')
+                description=j.find_all('td')
+                if heading is not None:
+                    for x in heading:
+                        list1.append(x.text)
+                if description is not None:
+                    for y in description:
+                        list2.append(y.text)
+                    # result['b']=f"{x.text} :: {y.text} \n"
+            l=tuple(zip(list1,list2))
+        a=[]
+        for i in range(len(l)):
+            a.append("::".join(l[i]))
+        a="\n".join(a)
+        print(a)
+
+        lst=[]
+        for k in range(1,16):
+            c=soup.find_all('p')[k].text
+            lst.append(c)
+        # print(lst)
+        # ,"lst":lst,"l":l
+        # ,"lst":lst
+    return render(request, 'core/home.html', {'result': result,"lst":lst,"l":a})
