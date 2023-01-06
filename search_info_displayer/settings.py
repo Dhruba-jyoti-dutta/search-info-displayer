6a4de9f91a2d403a5d95836fcfa6944d0cec486b
Dhruba-jyoti-dutta <sddhruba.857@gmail.com>
2023-01-07 01:36:48 +0530

    allowed hosts

--- search_info_displayer/settings.py
+++ search_info_displayer/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'bkr(1mx&+l80n275l=j6rz!4@qzfb$^-n86xqc*+sj9b*kz9gm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
